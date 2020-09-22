ff9b6f730aad49188c6f821fe3824f053ec8a5f1
Jack Ellis <jackellis1504@gmail.com>
2020-09-22 18:35:51 +0100

    Updated heavily such that you can now create and edit trainings in the same form

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -26,8 +26,9 @@ class TrainingController extends Controller
      */
     public function create()
     {
+        $training = new Training();
         $other_training = Training::all();
-        return view('training.create', compact('other_training'));
+        return view('training.create', compact('training', 'other_training'));
     }
 
     /**
@@ -41,16 +42,16 @@ class TrainingController extends Controller
         $training = new Training([
             'name' => $request->input('name')
         ]);
-        if($training->save()) {
-            foreach($request->input('prerequisite') as $prereq_id) {
+        if ($training->save()) {
+            foreach ($request->input('prerequisite') as $prereq_id) {
                 $prereq = new Prerequisite([
                     'training_id' => $training->id,
                     'prerequisite_id' => $prereq_id
                 ]);
                 $prereq->save();
             }
+            return redirect()->route('training.index');
         }
-
     }
 
     /**
@@ -72,7 +73,8 @@ class TrainingController extends Controller
      */
     public function edit(Training $training)
     {
-        //
+        $other_training = Training::all();
+        return view('training.create', compact('training', 'other_training'));
     }
 
     /**
@@ -84,7 +86,32 @@ class TrainingController extends Controller
      */
     public function update(Request $request, Training $training)
     {
-        //
+        $training->update($request->all());
+        if ($training->save()) {
+            /*
+                Find those prerequisites in the database
+                whose training ID = $training->id, but whose
+                prerequisite_id is not in the array of prerequisite IDs given
+            */
+            $prerequisite_ids = $request->input('prerequisite');
+            $useless = Prerequisite::where('training_id', $training->id)
+            ->whereNotIn('prerequisite_id', $prerequisite_ids)
+            ->delete();
+            foreach ($prerequisite_ids as $prereq_id) {
+                $prereq = Prerequisite::where([
+                    ['training_id', '=', $training->id],
+                    ['prerequisite_id', '=', $prereq_id]
+                ])->first();
+                if ($prereq === null) {
+                    $prereq = new Prerequisite([
+                        'training_id' => $training->id,
+                        'prerequisite_id' => $prereq_id
+                    ]);
+                }
+                $prereq->save();
+            }
+            return redirect()->route('training.index');
+        }
     }
 
     /**
