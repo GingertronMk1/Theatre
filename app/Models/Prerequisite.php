e6ac2bf6183cc77a5d7d2f318d33e6614c2add3c
Jack Ellis <jackellis1504@gmail.com>
2020-09-22 17:00:22 +0100

    Prereqs for training

--- app/Models/Prerequisite.php
+++ app/Models/Prerequisite.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Prerequisite extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'training_id',
+        'prerequisite_id'
+    ];
+
+    protected $with = [
+        'training',
+        'prerequisite'
+    ];
+
+    public function training() {
+        return $this->belongsTo('App\Models\Training', 'training_id');
+    }
+
+    public function prerequisite() {
+        return $this->belongsTo('App\Models\Training', 'prerequisite_id');
+    }
+
 }
