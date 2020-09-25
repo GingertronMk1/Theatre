258f0003f649f7782da08e9e388759ffffe4ba0f
Jack Ellis <jackellis1504@gmail.com>
2020-09-25 11:06:00 +0100

    Added big scary relationships for cast and crew

--- app/Models/Show.php
+++ app/Models/Show.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,32 @@ class Show extends Model
 
     public function instances() {
         return $this->hasMany(ShowInstance::class);
+    }
+
+    public function showRoles() {
+        return $this->hasMany(ShowRole::class);
+    }
+
+    public function roles() {
+        return $this->belongsToMany(Role::class, ShowRole::class)->withPivot(['user_id']);
+    }
+
+    public function cast() {
+        $sectionId = RoleSection::where('name', '=', RoleSection::NAME_CAST)->first()->id;
+        return $this
+        ->showRoles()
+        ->with(['user', 'role'])
+        ->whereHas('role', function(Builder $query) use ($sectionId) {
+            $query->where('role_section_id', $sectionId);
+        });
+    }
+    public function crew() {
+        $sectionId = RoleSection::where('name', '=', RoleSection::NAME_CREW)->first()->id;
+        return $this
+        ->showRoles()
+        ->with(['user', 'role'])
+        ->whereHas('role', function(Builder $query) use ($sectionId) {
+            $query->where('role_section_id', $sectionId);
+        });
     }
 }
