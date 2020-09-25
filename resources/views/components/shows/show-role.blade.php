b84d2783c1a51c7d7c0c6ae7c73f306ab40b284f
Jack Ellis <jackellis1504@gmail.com>
2020-09-25 11:06:19 +0100

    Updated show role component with nullable values for new entries

--- resources/views/components/shows/show-role.blade.php
+++ resources/views/components/shows/show-role.blade.php
@@ -1,13 +1,23 @@
+@php
+$user_id = $user_id ?? null;
+$role_id = $role_id ?? null;
+@endphp
+
+
 <div class="form-group form-inline new-role">
-    <select class="form-control" name="roles[][role_id]">
+    <select class="form-control mr-3" name="roles[role_id][]">
         @foreach(App\Models\Role::all() as $role)
-        <option value="{{ $role->id }}">{{ $role->name }}</option>
+        <option value="{{ $role->id }}" {{ $role->id === $role_id ? 'selected' : '' }}>
+            {{ $role->name }}
+        </option>
         @endforeach
     </select>
 
-    <select class="form-control" name="roles[][user_id]">
+    <select class="form-control" name="roles[user_id][]">
         @foreach(App\Models\User::all() as $user)
-        <option value="{{ $user->id }}">{{ $user->name }}</option>
+        <option value="{{ $user->id }}" {{ $user->id === $user_id ? 'selected' : '' }}>
+            {{ $user->name }}
+        </option>
         @endforeach
     </select>
 </div>
